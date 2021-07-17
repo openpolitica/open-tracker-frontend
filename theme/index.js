dc0ae1c8bed3ddce48890ba1fe8adc5e1a88867e
Paul Ccari <paulclindo@gmail.com>
2021-07-16 23:49:17 -0500

    add breakpoints to theme

--- theme/index.js
+++ theme/index.js
@@ -1,4 +1,5 @@
 import { extendTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 import { ButtonStyles as Button } from 'components/ButtonStyles';
 import { globalStyles } from 'theme/globalStyles';
 
@@ -6,8 +7,16 @@ const components = {
   Button,
 };
 
+const breakpoints = createBreakpoints({
+  sm: '20em',
+  md: '48em',
+  lg: '62em',
+  xl: '90em',
+});
+
 const themeDefault = {
   components,
+  breakpoints,
   styles: {
     global: globalStyles,
   },
