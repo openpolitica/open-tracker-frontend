0eaa31e20d9ade1b85679beb9241dd5bd5e95e79
Paul Ccari <paulclindo@gmail.com>
2021-07-16 22:29:11 -0500

    add Inter variable font, preload and cache it

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'theme';
+import Fonts from 'theme/fonts';
 
 function MyOPApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme} resetCSS>
+      <Fonts />
       <Component {...pageProps} />
     </ChakraProvider>
   );
