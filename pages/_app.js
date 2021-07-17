a222f2a52ffb14a813e24c21874d72291307d6f5
Paul Ccari <paulclindo@gmail.com>
2021-07-16 23:32:09 -0500

    move title meta to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import Head from 'next/head';
 import theme from 'theme';
 import Fonts from 'theme/fonts';
 
 function MyOPApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme} resetCSS>
-      <Fonts />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Open Tracker</title>
+      </Head>
+      <ChakraProvider resetCSS theme={theme}>
+        <Fonts />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
