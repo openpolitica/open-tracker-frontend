24cd3575e01e46b5cb440f566c92613589ffbb1e
Paul Ccari <paulclindo@gmail.com>
2021-08-05 20:37:55 -0500

    refactor breadcrumb using NextCUILink

--- components/Breadcrumb/index.js
+++ components/Breadcrumb/index.js
@@ -1,5 +1,5 @@
 import * as CUI from '@chakra-ui/react';
-import NextLink from 'next/link';
+import NextCUILink from 'components/NextCUILink';
 import ArrowRightIcon from '/public/images/icons/arrow-right.svg';
 
 export default function Breadcrumb({ routes = [] }) {
@@ -9,12 +9,12 @@ export default function Breadcrumb({ routes = [] }) {
     <CUI.Breadcrumb separator={<ArrowRightIcon />} spacing="0.25rem">
       {routes.map(({ label, route }, idx) => (
         <CUI.BreadcrumbItem key={label}>
-          <CUI.BreadcrumbLink as={NextLink} href={route}>
-            <CUI.Link
-              color={idx === lastIdxRoute ? 'unset' : 'secondary.600'}
-              pointerEvents={idx === lastIdxRoute ? 'none' : 'auto'}>
-              {label}
-            </CUI.Link>
+          <CUI.BreadcrumbLink
+            as={NextCUILink}
+            href={route}
+            color={idx === lastIdxRoute ? 'unset' : 'secondary.600'}
+            pointerEvents={idx === lastIdxRoute ? 'none' : 'auto'}>
+            {label}
           </CUI.BreadcrumbLink>
         </CUI.BreadcrumbItem>
       ))}
