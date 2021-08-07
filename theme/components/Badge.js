f9671090f46d3b2246b26a44aa42598224c74513
Paul Ccari <paulclindo@gmail.com>
2021-08-06 20:06:28 -0500

    add badge variant and styles to theme

---
+++ theme/components/Badge.js
@@ -0,0 +1,20 @@
+export const BadgeStyles = {
+  baseStyle: {
+    borderRadius: '6px',
+    fontSize: 'xs',
+    px: 2,
+    py: 1,
+    fontWeight: '500',
+    textTransform: 'unset',
+  },
+  variants: {
+    success: {
+      bg: 'primary.500',
+      color: 'white',
+    },
+    danger: {
+      bg: 'red.500',
+      color: 'white',
+    },
+  },
+};
