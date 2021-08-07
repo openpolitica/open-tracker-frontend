8bb683e0191410fa6b605782f43907d19dd284d3
Paul Ccari <paulclindo@gmail.com>
2021-08-06 20:08:22 -0500

    show congressmanCard in Congressmen Page

--- pages/congresistas/index.js
+++ pages/congresistas/index.js
@@ -1,9 +1,11 @@
 import SidebarLayout from 'components/layout/SidebarLayout';
+import CongressmanProfileCard from 'components/CongressmanProfileCard';
 
 export default function Congresspeople() {
   return (
     <SidebarLayout>
       <h1>Congresistas</h1>
+      <CongressmanProfileCard />
     </SidebarLayout>
   );
 }
