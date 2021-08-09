af34b89798a27e48067cd2d61699ca1e46bbe08f
almeald <almenaldana@gmail.com>
2021-08-09 13:35:45 -0500

    show parlamentaryGropCard in Home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import SidebarLayout from 'components/layout/SidebarLayout';
+import ParliamentaryGroupCard from 'components/ParliamentaryGroupCard';
 
 export default function Home() {
   return (
     <SidebarLayout>
       <h1>HomePage</h1>
+      <ParliamentaryGroupCard />
     </SidebarLayout>
   );
 }
