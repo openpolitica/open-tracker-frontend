3eecf29271b609666cf821d415792f196d204f06
almeald <almenaldana@gmail.com>
2021-08-09 20:07:00 -0500

    show congresspersonCard in home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import SidebarLayout from 'components/layout/SidebarLayout';
 import ParliamentaryGroupCard from 'components/ParliamentaryGroupCard';
+import CongresspersonCard from 'components/CongresspersonCard';
 
 export default function Home() {
   return (
     <SidebarLayout>
       <h1>HomePage</h1>
       <ParliamentaryGroupCard />
+      <CongresspersonCard />
     </SidebarLayout>
   );
 }
