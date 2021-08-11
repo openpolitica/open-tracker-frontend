a1bf8a5eb4fb5396585616e10a773f8b72ed6b63
Paul Ccari <paulclindo@gmail.com>
2021-08-10 22:45:16 -0500

    fix component naming to CongresspersonProfileCard

--- pages/congresistas/index.js
+++ pages/congresistas/index.js
@@ -1,11 +1,11 @@
 import SidebarLayout from 'components/layout/SidebarLayout';
-import CongressmanProfileCard from 'components/CongressmanProfileCard';
+import CongresspersonProfileCard from 'components/CongresspersonProfileCard';
 
 export default function Congresspeople() {
   return (
     <SidebarLayout>
       <h1>Congresistas</h1>
-      <CongressmanProfileCard />
+      <CongresspersonProfileCard />
     </SidebarLayout>
   );
 }
