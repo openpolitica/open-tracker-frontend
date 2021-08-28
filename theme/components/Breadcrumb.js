4472e1b5470df29acfaaba06e2bb11c65e3be31e
Paul Ccari <paulclindo@gmail.com>
2021-08-28 18:33:20 -0500

    update breadcrumb styles

--- theme/components/Breadcrumb.js
+++ theme/components/Breadcrumb.js
@@ -2,14 +2,14 @@ export const BreadcrumbStyles = {
   parts: ['container'],
   baseStyle: {
     container: {
-      border: '1px solid',
-      borderColor: 'secondary.200',
+      bg: 'secondary.50',
       borderRadius: '4px',
       color: 'secondary.700',
       display: 'inline-flex',
       fontSize: 'xs',
       lineHeight: 'none',
-      p: '4',
+      px: '4',
+      py: '3',
     },
   },
 };
