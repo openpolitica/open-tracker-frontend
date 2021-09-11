4bcc52b0048f457dc4e12c079ff9df5effa2ed94
Juan La Jara <juanlajara@gmail.com>
2021-09-11 17:46:17 -0500

    Add default icon to `ParliamentaryGroupCard` component
    We also update prop names and default values.

--- components/ParliamentaryGroupCard/index.js
+++ components/ParliamentaryGroupCard/index.js
@@ -2,10 +2,10 @@ import * as CUI from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 
 const ParliamentaryGroupCard = ({
-  logoParty = 'https://cdn.mercadonegro.pe/wp-content/uploads/2020/01/congreso-2020-alianza-para-el-progreso-1024x1024.png',
-  nameParty = 'Alianza por el Progreso',
-  members = '12',
-  parliamentaryGroupId = 'alianza-por-el-progreso',
+  partyLogoURL = 'images/icons/parliamentary-group-default.svg',
+  partyName = 'default-value',
+  members = '0',
+  parliamentaryGroupId = 'default-value',
 }) => {
   const router = useRouter();
   return (
@@ -20,13 +20,13 @@ const ParliamentaryGroupCard = ({
       border="1px solid"
       borderColor="secondary.200"
       rounded="4">
-      <CUI.Image w="16" mx="auto" mb="4" src={logoParty} />
+      <CUI.Image w="16" mx="auto" mb="4" src={partyLogoURL} />
       <CUI.Heading
         fontSize="sm"
         lineHeight="5"
         fontWeight="medium"
         color="secondary.700">
-        {nameParty}
+        {partyName}
       </CUI.Heading>
       <CUI.Text color="gray.500" mt="1" fontSize="sm">
         {members} {members === '1' ? 'miembro' : 'miembros'}
