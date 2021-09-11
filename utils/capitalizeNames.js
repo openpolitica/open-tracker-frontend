06df50ed8c7ab20263d90a9b1bded9c68f3d5d14
Paul Ccari <paulclindo@gmail.com>
2021-09-11 02:24:45 -0500

    exclude some specific words in capitalizeNames

--- utils/capitalizeNames.js
+++ utils/capitalizeNames.js
@@ -1,7 +1,14 @@
-export const capitalizeNames = (string = '') =>
-  string
+export const capitalizeNames = (
+  string = '',
+  { exclude = ['de', 'en', 'el', 'la', 'del', 'los'] } = {},
+) => {
+  const excludeRegex = new RegExp('^(?!(' + exclude.join('|') + ')\\b)\\w');
+  return string
     .trim()
     .toLowerCase()
-    .replace(/\w\S*/g, word =>
-      word.replace(/^\w/, firstLetter => firstLetter.toUpperCase()),
+    .replace(/\w\S*/g, (word, idx) =>
+      idx === 0
+        ? word.replace(/^\w/, firstLetter => firstLetter.toUpperCase())
+        : word.replace(excludeRegex, firstLetter => firstLetter.toUpperCase()),
     );
+};
