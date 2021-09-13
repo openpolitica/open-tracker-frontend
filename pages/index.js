52812391f54d4d6e257d85a9a8d3521bc121db3d
Juan La Jara <juanlajara@gmail.com>
2021-09-12 20:00:53 -0500

    Redirect root route to `/bancadas`

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
 import SidebarLayout from 'components/layout/SidebarLayout';
-import ParliamentaryGroupCard from 'components/ParliamentaryGroupCard';
-import CongresspersonCard from 'components/CongresspersonCard';
 
 export default function Home() {
-  return (
-    <SidebarLayout>
-      <h1>HomePage</h1>
-      <ParliamentaryGroupCard />
-      <CongresspersonCard />
-    </SidebarLayout>
-  );
+  return <SidebarLayout></SidebarLayout>;
 }
