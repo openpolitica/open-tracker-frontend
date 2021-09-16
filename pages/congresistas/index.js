66d82958cebc3d56b8474c834129f775d53b51de
Juan La Jara <juanlajara@gmail.com>
2021-09-15 21:46:01 -0500

    Add `filterCongresspeopleByElectoralDistrict` to congresistas page
    And use it.

--- pages/congresistas/index.js
+++ pages/congresistas/index.js
@@ -1,14 +1,48 @@
+import { useState, useEffect } from 'react';
 import * as CUI from '@chakra-ui/react';
 import SidebarLayout from 'components/layout/SidebarLayout';
 import CongresspersonCard from 'components/CongresspersonCard';
 import Breadcrumb from 'components/Breadcrumb';
+import { capitalizeNames } from 'utils';
 
 const routes = [
   { label: 'Inicio', route: '/' },
   { label: 'Congresistas', route: '/congresistas' },
 ];
 
+const electoralDistrict = congressperson =>
+  congressperson?.location?.location_name?.toLowerCase();
+
+const electoralDistrictsFromCongresspeople = congresspeople =>
+  congresspeople
+    .reduce(
+      (prev, current) =>
+        prev.includes(electoralDistrict(current))
+          ? prev
+          : [...prev, electoralDistrict(current)],
+      [],
+    )
+    .sort();
+
+const filterCongresspeopleByElectoralDistrict = (congresspeople, district) => {
+  if (!district) {
+    return congresspeople;
+  }
+  return congresspeople.filter(
+    congressperson => electoralDistrict(congressperson) === district,
+  );
+};
+
 export default function Congresspeople({ congresspeople }) {
+  const [filterSubset, setFilterSubset] = useState([]);
+  const [districtFilter, setDistrictFilter] = useState(null);
+
+  useEffect(() => {
+    setFilterSubset(
+      filterCongresspeopleByElectoralDistrict(congresspeople, districtFilter),
+    );
+  }, [districtFilter, congresspeople]);
+
   return (
     <SidebarLayout>
       <Breadcrumb routes={routes} />
@@ -23,9 +57,26 @@ export default function Congresspeople({ congresspeople }) {
       <CUI.Text mb="5" color="secondary.500">
         Estos son los congresistas que te representarán durante este periodo.
       </CUI.Text>
-      {/* TODO: Add select component and isSuspenderMember */}
+      {/* TODO: Add isSuspenderMember */}
+      <CUI.Text color="secondary.700" lineHeight="6" mb="2">
+        Filtra por departamento
+      </CUI.Text>
+      <CUI.Select
+        onChange={event => setDistrictFilter(event.target.value)}
+        cursor="pointer"
+        w="64"
+        mb="4">
+        <option key="no-select" value="">
+          Ver de todo el país
+        </option>
+        {electoralDistrictsFromCongresspeople(congresspeople).map(district => (
+          <option key={district} value={district.toLowerCase()}>
+            {capitalizeNames(district)}
+          </option>
+        ))}
+      </CUI.Select>
       <CUI.Wrap spacing="4">
-        {congresspeople.map(congressperson => (
+        {filterSubset.map(congressperson => (
           <CUI.WrapItem key={congressperson.cv_id}>
             <CongresspersonCard
               congresspersonSlug={congressperson.congressperson_slug}
