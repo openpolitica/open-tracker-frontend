cf2a65677318470929b64ec5b21e7535179de4b1
redsii <77481029+redsii@users.noreply.github.com>
2021-09-23 21:08:32 +0200

    Revert "Update favicon to 🦉"

--- pages/_document.js
+++ pages/_document.js
@@ -20,30 +20,11 @@ export default class MyDocument extends Document {
             crossOrigin="anonymous"
           />
           <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
+            href="favicon/favicon.ico"
             rel="icon"
             type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
+            sizes="48x48"
           />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link
-            rel="mask-icon"
-            href="/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
-          <meta name="msapplication-TileColor" content="#da532c" />
-          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
