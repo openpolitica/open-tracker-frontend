26742659fa36cc12974893d7456cfd060c286fda
Paul Ccari <paulclindo@gmail.com>
2021-09-27 19:37:56 -0500

    add google analytics for tracking page views

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
-import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import { Fragment, useEffect } from 'react';
 import theme from 'theme';
 import Fonts from 'theme/fonts';
+import { pageview } from 'lib/gtag';
 
 function MyOPApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <Fragment>
       <Head>
