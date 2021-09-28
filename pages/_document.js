1fca8a0440592aa5916c7d621ac4ce4c56117e3a
Paul Ccari <paulclindo@gmail.com>
2021-09-27 19:37:56 -0500

    add google analytics for tracking page views

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,22 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
           <link
             rel="preload"
             href="/fonts/inter-var-latin.woff2"
