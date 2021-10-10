6080519a25eb4daa68b3bf29303338b17a4b4fe1
almeald <almenaldana@gmail.com>
2021-10-10 03:29:39 -0500

    parliamentary group card in mobile version

--- components/ParliamentaryGroupCard/index.js
+++ components/ParliamentaryGroupCard/index.js
@@ -11,8 +11,9 @@ const ParliamentaryGroupCard = ({
   return (
     <CUI.Flex
       direction="column"
-      maxW="44"
-      minH="64"
+      w={{ base: '72', md: '44' }}
+      maxW={{ base: '72', md: '44' }}
+      minH={{ base: '44', md: '64' }}
       px="5"
       py="6"
       textAlign="center"
@@ -20,17 +21,26 @@ const ParliamentaryGroupCard = ({
       border="1px solid"
       borderColor="secondary.200"
       rounded="4">
-      <CUI.Image w="16" mx="auto" mb="4" src={partyLogoURL} />
-      <CUI.Heading
-        fontSize="sm"
-        lineHeight="5"
-        fontWeight="medium"
-        color="secondary.700">
-        {partyName}
-      </CUI.Heading>
-      <CUI.Text color="gray.500" mt="1" fontSize="sm">
-        {members} {members === '1' ? 'miembro' : 'miembros'}
-      </CUI.Text>
+      <CUI.Flex direction={{ base: 'row', md: 'column' }} align="center">
+        <CUI.Image
+          w="16"
+          mx={{ base: '0', md: 'auto' }}
+          mb={{ base: '0', md: '4' }}
+          src={partyLogoURL}
+        />
+        <CUI.Box flex="1">
+          <CUI.Heading
+            fontSize="sm"
+            lineHeight="5"
+            fontWeight="medium"
+            color="secondary.700">
+            {partyName}
+          </CUI.Heading>
+          <CUI.Text color="gray.500" mt="1" fontSize="sm">
+            {members} {members === '1' ? 'miembro' : 'miembros'}
+          </CUI.Text>
+        </CUI.Box>
+      </CUI.Flex>
       <CUI.Button
         variant="outline"
         mt="auto"
