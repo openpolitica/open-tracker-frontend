11917237df546c8e62908b7693596ed4de89e064
almeald <almenaldana@gmail.com>
2021-10-10 02:30:07 -0500

    congressperson card in mobile version

--- components/CongresspersonCard/index.js
+++ components/CongresspersonCard/index.js
@@ -18,62 +18,68 @@ const CongresspersonCard = ({
     <CUI.Flex
       direction="column"
       align="center"
-      w="44"
-      maxW="44"
-      minH="72"
-      px="4"
+      w={{ base: '72', md: '44' }}
+      h={{ base: '56', md: '72' }}
+      maxW={{ base: '72', md: '44' }}
+      minH={{ base: '56', md: '72' }}
+      px={{ base: '8', md: '4' }}
       py="6"
-      textAlign="center"
       boxShadow="sm"
       border="1px solid"
       borderColor="secondary.200"
       rounded="4">
-      <CUI.Avatar
-        w="16"
-        h="16"
-        src={avatar}
-        name={fullName}
-        objectFit="contain">
-        <CUI.AvatarBadge
-          as="img"
-          src={logoParty}
-          boxSize="8"
-          border="2px solid"
-        />
-      </CUI.Avatar>
-      <CUI.Heading
-        fontSize="sm"
-        fontWeight="medium"
-        color="secondary.700"
-        my="2">
-        {capitalizeNames(fullName ?? '')}
-      </CUI.Heading>
-      {isSpeaker ? (
-        <CUI.Badge py="0.5" mb="1.5" variant="success">
-          {gender === 'M' ? 'Vocero' : 'Vocera'} de la bancada
-        </CUI.Badge>
-      ) : null}
-      {isSuspendedMember ? (
-        <CUI.Badge variant="danger">Suspendida</CUI.Badge>
-      ) : null}
-      <CUI.Text
-        fontSize="sm"
-        color="secondary.500"
-        lineHeight="5"
-        mt="2"
-        mb="4">
-        Congresista por{' '}
-        <NextCUILink
-          href={`/congresistas?departamento=${location.toLowerCase()}`}
-          display="block"
-          color="primary.500">
-          {capitalizeNames(location ?? '')}
-        </NextCUILink>
-      </CUI.Text>
+      <CUI.Flex direction={{ base: 'row', md: 'column' }} align="center">
+        <CUI.Avatar
+          w="16"
+          h="16"
+          src={avatar}
+          name={fullName}
+          objectFit="contain">
+          <CUI.AvatarBadge as="img" src={logoParty} boxSize="8" />
+        </CUI.Avatar>
+        <CUI.Box
+          ml={{ base: '4', md: '0' }}
+          textAlign={{ base: 'initial', md: 'center' }}>
+          <CUI.Heading
+            fontSize="sm"
+            fontWeight="medium"
+            color="secondary.700"
+            my={{ base: '0', md: '2' }}>
+            {capitalizeNames(fullName ?? '')}
+          </CUI.Heading>
+          {isSpeaker ? (
+            <CUI.Badge
+              py="0.5"
+              mb="1.5"
+              mt={{ base: '1.5', md: '0' }}
+              variant="success">
+              {gender === 'M' ? 'Vocero' : 'Vocera'} de la bancada
+            </CUI.Badge>
+          ) : null}
+          {isSuspendedMember ? (
+            <CUI.Badge variant="danger">Suspendida</CUI.Badge>
+          ) : null}
+          <CUI.Text
+            fontSize="sm"
+            color="secondary.500"
+            lineHeight="5"
+            mt={{ base: '0', md: '2' }}
+            mb={{ base: '0', md: '4' }}>
+            Congresista por{' '}
+            <NextCUILink
+              href={`/congresistas?departamento=${location.toLowerCase()}`}
+              display="block"
+              color="primary.500">
+              {capitalizeNames(location ?? '')}
+            </NextCUILink>
+          </CUI.Text>
+        </CUI.Box>
+      </CUI.Flex>
       <CUI.Button
         onClick={() => router.push(`/congresistas/${congresspersonSlug}`)}
         variant="outline"
-        mt="auto">
+        mt="auto"
+        w={{ base: 'full', md: 'auto' }}>
         Ver perfil
       </CUI.Button>
     </CUI.Flex>
