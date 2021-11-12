dac20fc4adca9b1dbe1a68cbfea345398c88b0d3
AlmeAld <almenaldana@gmail.com>
2021-11-11 20:38:30 -0500

    add footer component

---
+++ components/layout/Footer.js
@@ -0,0 +1,119 @@
+import * as CUI from '@chakra-ui/react';
+import NextCUILink from 'components/NextCUILink';
+import TwitterFooterIcon from 'public/images/icons/twitter-footer.svg';
+import LinkedinIcon from 'public/images/icons/linkedin.svg';
+import InstagramIcon from 'public/images/icons/instagram.svg';
+
+const SocialButton = ({ children, label, href }) => {
+  return (
+    <CUI.Link
+      isExternal
+      w="8"
+      h="8"
+      cursor="pointer"
+      href={href}
+      display="inline-flex"
+      alignItems="center"
+      justifyContent="center"
+      transition="'background 0.3s ease">
+      <CUI.VisuallyHidden>{label}</CUI.VisuallyHidden>
+      {children}
+    </CUI.Link>
+  );
+};
+
+export default function Footer() {
+  return (
+    <CUI.Box
+      as="footer"
+      bg="gray.100"
+      minh="64"
+      zIndex="docked"
+      py={{ base: '6', md: '10' }}
+      pr={{ base: '6', md: '10' }}
+      pl={{ base: '6', md: 'calc(var(--chakra-sizes-sidebarWidth) + 6px)' }}>
+      <CUI.Flex
+        direction={{ base: 'column', md: 'row' }}
+        justifyContent="space-between">
+        <CUI.Stack spacing="2" maxW="32%">
+          <CUI.Text
+            as={NextCUILink}
+            href="/"
+            lineHeight="9"
+            fontFamily="ClashDisplay"
+            fontSize="3xl"
+            fontWeight="bold">
+            Tuku
+          </CUI.Text>
+          <CUI.Text fontSize="sm" color="gray.500">
+            La plataforma donde podrás encontrar información y hacer seguimiento
+            a las actividades del Congreso de la República del Perú.
+          </CUI.Text>
+          <CUI.Box direction="row" spacing="6">
+            <SocialButton
+              label="Linkedin"
+              href={
+                'https://www.linkedin.com/company/open-pol%C3%ADtica/mycompany/'
+              }>
+              <LinkedinIcon />
+            </SocialButton>
+            <SocialButton
+              label="Twitter"
+              href={'https://twitter.com/openpolitica?lang=es'}>
+              <TwitterFooterIcon />
+            </SocialButton>
+            <SocialButton
+              label="Instagram"
+              href={'https://www.instagram.com/open.politica/?hl=es'}>
+              <InstagramIcon />
+            </SocialButton>
+          </CUI.Box>
+          <CUI.Text fontSize="sm" color="gray.500">
+            © 2021 Open Política. Todos los derechos reservados.
+          </CUI.Text>
+        </CUI.Stack>
+        <CUI.Flex
+          position="relative"
+          justifyContent="space-between"
+          maxW="60%"
+          flexWrap="wrap">
+          <CUI.Stack
+            pr="12"
+            _after={{
+              base: 'none',
+              md: {
+                content: "''",
+                width: '1px',
+                background: '#CBD5E0',
+                position: 'absolute',
+                height: '56px',
+                top: '0',
+                left: '80',
+              },
+            }}>
+            <CUI.Text fontWeight="semibold" color="gray.600">
+              Ayúdanos a seguir creando soluciones:
+            </CUI.Text>
+            <CUI.Link
+              isExternal
+              color="primary.500"
+              href="https://www.patreon.com/openpolitica">
+              patreon.com/openpolitica
+            </CUI.Link>
+          </CUI.Stack>
+          <CUI.Stack>
+            <CUI.Text fontWeight="semibold" color="gray.600">
+              ¿Tienes alguna duda?
+            </CUI.Text>
+            <CUI.Link
+              href="mailto:hola@openpolitica.com"
+              color="primary.500"
+              mt={{ base: '0', md: 'initial' }}>
+              hola@openpolitica.com
+            </CUI.Link>
+          </CUI.Stack>
+        </CUI.Flex>
+      </CUI.Flex>
+    </CUI.Box>
+  );
+}
--- components/layout/SidebarLayout/index.js
+++ components/layout/SidebarLayout/index.js
@@ -1,40 +1,45 @@
 import * as CUI from '@chakra-ui/react';
+import { Fragment } from 'react';
 import SidebarContent from './SidebarContent';
 import MobileNav from './MobileNav';
 import { DesktopNav } from './DesktopNav';
+import Footer from '../Footer';
 
 export default function SidebarLayout({ children }) {
   const { isOpen, onToggle, onClose } = CUI.useDisclosure();
 
   return (
-    <CUI.Box minH="100vh" bg="white">
-      <DesktopNav display={{ base: 'none', md: 'block' }} />
-      <CUI.Box>
-        <SidebarContent display={{ base: 'none', md: 'block' }} />
-        <CUI.Drawer
-          autoFocus={false}
-          placement="left"
-          isOpen={isOpen}
-          onClose={onClose}
-          onOverlayClick={onClose}>
-          <CUI.DrawerContent maxW="sidebarWidth" boxShadow="unset" mt="14">
-            <SidebarContent />
-          </CUI.DrawerContent>
-        </CUI.Drawer>
-        <MobileNav
-          display={{ base: 'flex', md: 'none' }}
-          isOpen={isOpen}
-          onToggle={onToggle}
-        />
-        <CUI.Box
-          ml={{ base: '0', md: '11.5rem' }}
-          mt={{ base: '0', md: '16' }}
-          px={{ base: '4', md: '8' }}
-          py={{ base: '4', md: '6' }}
-          pt={{ base: '4.5rem', md: '6' }}>
-          {children}
+    <Fragment>
+      <CUI.Box minH="100vh" bg="white">
+        <DesktopNav display={{ base: 'none', md: 'block' }} />
+        <CUI.Box>
+          <SidebarContent display={{ base: 'none', md: 'block' }} />
+          <CUI.Drawer
+            autoFocus={false}
+            placement="left"
+            isOpen={isOpen}
+            onClose={onClose}
+            onOverlayClick={onClose}>
+            <CUI.DrawerContent maxW="sidebarWidth" boxShadow="unset" mt="14">
+              <SidebarContent />
+            </CUI.DrawerContent>
+          </CUI.Drawer>
+          <MobileNav
+            display={{ base: 'flex', md: 'none' }}
+            isOpen={isOpen}
+            onToggle={onToggle}
+          />
+          <CUI.Box
+            ml={{ base: '0', md: '11.5rem' }}
+            mt={{ base: '0', md: '16' }}
+            px={{ base: '4', md: '8' }}
+            py={{ base: '4', md: '6' }}
+            pt={{ base: '4.5rem', md: '6' }}>
+            {children}
+          </CUI.Box>
         </CUI.Box>
       </CUI.Box>
-    </CUI.Box>
+      <Footer />
+    </Fragment>
   );
 }
