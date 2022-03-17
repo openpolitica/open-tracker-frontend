9fa68752de0e8d2d4e3f80f3f29e6e02f05f0ad8
Luighi <luighiavz@gmail.com>
2022-03-16 20:38:48 -0500

    Adjust relationship between api path and environment
    Make less dependant on name and change the condition to enable hotjar in
    other environments than local (including current dev environment)

--- pages/_app.js
+++ pages/_app.js
@@ -56,6 +56,6 @@ const withHotjar = Component => props => {
   return <Component {...props} />;
 };
 
-export default process.env.NEXT_PUBLIC_ENVIRONMENT === 'dev'
+export default process.env.NEXT_PUBLIC_ENVIRONMENT === 'local'
   ? Tracker
   : withHotjar(Tracker);
