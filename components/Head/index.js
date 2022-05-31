d147fc6e42144cce66302a89f05b8e54b7c344f1
Luighi <luighiavz@gmail.com>
2022-05-30 22:02:12 -0500

    Avoid duplicated head tags
    Use keys in each tag to avoid duplication https://nextjs.org/docs/api-reference/next/head

--- components/Head/index.js
+++ components/Head/index.js
@@ -4,20 +4,28 @@ export default function DynamicHead(meta) {
   return (
     <Head>
       <title>{meta.title}</title>
-      <meta name="robots" content="follow, index" />
-      <meta content={meta.description} name="description" />
-      <meta property="og:url" content={meta.websiteUrl} />
-      <link rel="canonical" href={meta.websiteUrl} />
-      <meta property="og:type" content={meta.type} />
-      <meta property="og:site_name" content={meta.title} />
-      <meta property="og:description" content={meta.description} />
-      <meta property="og:title" content={meta.title} />
-      <meta property="og:image" content={meta.image} />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@openpolitica" />
-      <meta name="twitter:title" content={meta.title} />
-      <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
+      <meta name="robots" content="follow, index" key="robots" />
+      <meta content={meta.description} name="description" key="description" />
+      <meta property="og:url" content={meta.websiteUrl} key="og-url" />
+      <link rel="canonical" href={meta.websiteUrl} key="canonical" />
+      <meta property="og:type" content={meta.type} key="og-type" />
+      <meta property="og:site_name" content={meta.title} key="og-site-name" />
+      <meta
+        property="og:description"
+        content={meta.description}
+        key="org-description"
+      />
+      <meta property="og:title" content={meta.title} key="og-title" />
+      <meta property="og:image" content={meta.image} key="og-type" />
+      <meta name="twitter:card" content="summary_large_image" key="tw-card" />
+      <meta name="twitter:site" content="@openpolitica" key="tw-site" />
+      <meta name="twitter:title" content={meta.title} key="tw-title" />
+      <meta
+        name="twitter:description"
+        content={meta.description}
+        key="tw-description"
+      />
+      <meta name="twitter:image" content={meta.image} key="tw-image" />
     </Head>
   );
 }
